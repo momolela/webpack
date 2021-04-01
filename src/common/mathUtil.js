1b2e47fff11b515319a19b964e8ddc7c2f0344bb
momolela <1083910359@qq.com>
2021-04-01 22:31:15 +0800

    20210401

---
+++ src/common/mathUtil.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+function multiply(a, b) {
+  return Number(a) * Number(b);
+}
+
+export { add, multiply };
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { add, multiply } from "./common/mathUtil";
+
+console.log(add(1, 2));
+console.log(multiply(1, 2));
